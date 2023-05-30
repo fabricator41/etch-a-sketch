951eca98373a6cddaafb9a6a49e5eb6ad7e6f2f1
Emre Ilhan <emrilhan@gmail.com>
2023-05-30 16:32:36 +0300

    slider and square add function created

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
 const container = document.querySelector('.container');
 const button = document.querySelector('#btn');
+const slider = document.getElementById('slider');
+
+let squareNumber = 0;
 
 button.addEventListener('click', deneme);
+slider.addEventListener('input', deneme2);
+
+function deneme2() {
+  const value = document.getElementById('value');
+  value.textContent = `${this.value} x ${this.value}`;
+  squareNumber = Math.pow(this.value, 2);
+}
 
 function deneme() {
-  const square = document.createElement('div');
-  container.appendChild(square);
-  square.classList.add('square');
+  for (let i = 0; i < squareNumber; i++) {
+    const square = document.createElement('div');
+    container.appendChild(square);
+    square.classList.add('square');
+  }
 }
