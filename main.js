297d18353fcd0afa68e81723c71a32a891535a91
Emre Ilhan <emrilhan@gmail.com>
2023-05-31 13:21:24 +0300

    finish commit

--- main.js
+++ main.js
@@ -2,21 +2,41 @@ const container = document.querySelector('.container');
 const button = document.querySelector('#btn');
 const slider = document.getElementById('slider');
 
-let squareNumber = 0;
+let squareNumber = 256;
+createSquares();
 
-button.addEventListener('click', deneme);
-slider.addEventListener('input', deneme2);
+button.addEventListener('click', () => {
+  reset();
+  createSquares();
+});
+slider.addEventListener('input', sliderValue);
 
-function deneme2() {
+function sliderValue() {
   const value = document.getElementById('value');
   value.textContent = `${this.value} x ${this.value}`;
   squareNumber = Math.pow(this.value, 2);
 }
 
-function deneme() {
+function reset() {
+  while (container.hasChildNodes()) {
+    container.removeChild(container.firstChild);
+  }
+}
+
+function createSquares() {
+  let width = 800 / Math.sqrt(squareNumber);
+  let height = width;
+
   for (let i = 0; i < squareNumber; i++) {
     const square = document.createElement('div');
     container.appendChild(square);
     square.classList.add('square');
+    square.style.height = `${width}px`;
+    square.style.width = `${height}px`;
+    square.addEventListener('mouseover', black);
   }
+}
+
+function black() {
+  this.classList.add('black');
 }
